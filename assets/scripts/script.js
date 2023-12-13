5f74946938cbcf5094eadf1be9665dcf2b31dd72
Mateus A. Carvalho <mateusabreu94@hotmail.com>
2023-12-12 22:10:53 -0300

    Started to work in JS Today I started to work in index.js functionality. I added seven audios tag in index.html and initialized in variables in index.js to use in functions that will invoke when the event click and keyup is triggred.

---
+++ assets/scripts/script.js
@@ -0,0 +1,30 @@
+const drumBassEl = document.querySelector('#drum-bass');
+const hihatCymbalEl = document.querySelector('#hihat-cymbal');
+const snareDrumEl = document.querySelector('#snare-drum');
+const rideCymbalEl = document.querySelector('#ride-cymbal');
+const tomDrumEl = document.querySelector('#tom-drum');
+const attackCymbalEl = document.querySelector('#drum-bass');
+const floorTomEl = document.querySelector('#floor-tom');
+
+// Const with sound effects
+const drumBassSoundEffectEl = document.querySelector('#drum-bass-sound-effect');
+const hihatCymbalSoundEffectEl = document.querySelector('#hihat-cymbal-sound-effect');
+const snareDrumSoundEffectEl = document.querySelector('#snare-drum-sound-effect');
+const rideCymbalSoundEffectEl = document.querySelector('#ride-cymbal-sound-effect');
+const tomDrumSoundEffectEl = document.querySelector('#tom-drum-sound-effect');
+const attackCymbalSoundEffectEl = document.querySelector('#attack-cymbal-sound-effect');
+const floorTomSoundEffectEl = document.querySelector('#floor-tom-sound-effect');
+
+console.log(drumBassSoundEffectEl)
+
+document.addEventListener("click", (e) => {
+  console.log(e.target);
+})
+
+function playDrumBass () {}
+function playHihatCymbal () {}
+function playSnareDrum () {}
+function playRideCymbal () {}
+function playTomDrum () {}
+function playAttackCymbal () {}
+function playFloorTom () {}
